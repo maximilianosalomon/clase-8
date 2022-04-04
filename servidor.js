990bd3a4f131ad3b30943b0c7cb46ee6787a8805
maximilianosalomon <maxi.salomon@gmail.com>
2022-04-04 08:39:59 -0300

    modificacion de rutas

--- servidor.js
+++ servidor.js
@@ -1,39 +1,42 @@
 //init
 const express = require("express");
+
 const app = express();
-// app.use(express.json)
-// app.use(express.urlencoded({extended:true}))
+const routerProductos = express.Router();
+
+app.use("/productos", routerProductos);
+
+routerProductos.use(express.json());
 
 //server
-const PORT = process.env.port || 8080;
+const PORT = 8080;
 const server = app.listen(PORT, () => {
   console.log(`Servidor http escuchando en el puerto ${server.address().port}`);
 });
 server.on("error", (error) => console.log(`Error en servidor ${error}`));
 
 //rutas
-app.get("/api/productos", (req, res) => {
-  res.send("products");
+const productos = [];
+routerProductos.get("/api/productos", (req, res) => {
+  res.send(productos);
 });
 
 //get prod id
-app.get("/api/productos/:id", (req, res) => {
-    res.send(productoRandom);
-});
-
-//post prod id
-app.post("/api/productos/:id", (req, res) => {
-    res.send(productoRandom);
-});
-
-//put prod id
-app.put("/api/productos/:id", (req, res) => {
-    res.send(productoRandom);
-});
-
-//delete prod id
-app.delete("/api/productos/:id", (req, res) => {
+routerProductos.get("/api/productos/:id", (req, res) => {
   res.send(productoRandom);
 });
 
+//post prod id
+routerProductos.post("/api/productos/:id", (req, res) => {
+  res.send(productoRandom);
+});
 
+//put prod id
+routerProductos.put("/api/productos/:id", (req, res) => {
+  res.send(productoRandom);
+});
+
+//delete prod id
+routerProductos.delete("/api/productos/:id", (req, res) => {
+  res.send(productoRandom);
+});
